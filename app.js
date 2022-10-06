f162dffd769cf98ba607507e28eb4917e09fb9f2
Duvan <duvancho303@gmail.com>
2022-10-06 16:11:13 -0600

    adding more code

---
+++ app.js
@@ -0,0 +1,12 @@
+let toggleStatus = false;
+
+let toggleNav = function() {
+    let getSidebar = document.querySelector(".nav-side-bar");
+    let getSidebarList = document.querySelector(".nav-side-bar ul");
+    let getSidebarTitle = document.querySelector(".nav-side-bar span");
+    let getSidebarLinks = document.querySelectorAll(".nav-side-bar a");
+    
+    if ( toggleStatus === false) {
+        
+    }
+}
